a557242581bf5ce4646aa8bfb66d3156e53e8870
buketgencer <99204723+buketgencer@users.noreply.github.com>
2025-01-24 14:47:15 +0300

    Initial commit

--- src/main/java/com/turkcelll/ecommerce/repository/CartItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.turkcelll.ecommerce.repository;
-
-import com.turkcelll.ecommerce.entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
